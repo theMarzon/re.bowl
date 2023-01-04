a8cda481054ee889e3a533a9b74ad94f53a4a150
theMarzon <themarzon@gmail.com>
2023-01-04 00:26:23 -0400

    🎨 Archivos actualizados

---
+++ benchs/code.bench.ts
@@ -0,0 +1,56 @@
+import { describe, bench } from 'vitest';
+
+import ReBowl from '../src/index.js';
+
+describe('Code test', () => {
+
+    bench('Set entry', () => {
+
+        const cache = new ReBowl();
+
+        cache.set('A', 'Hello world');
+    });
+
+    bench('Set and clone entry', () => {
+
+        const cache = new ReBowl();
+
+        cache.set('A', 'Hello world');
+
+        cache.clone('A', 'B');
+    });
+
+    bench('Set and delete entry', () => {
+
+        const cache = new ReBowl();
+
+        cache.set('A', 'Hello world');
+
+        cache.delete('A');
+    });
+
+    bench('Set and has entry', () => {
+
+        const cache = new ReBowl();
+
+        cache.set('A', 'Hello world');
+
+        cache.has('A');
+    });
+
+    bench('Set and get entry', () => {
+
+        const cache = new ReBowl();
+
+        cache.set('A', 'Hello world');
+
+        cache.get('A');
+    });
+
+    bench('Get entries size', () => {
+
+        const cache = new ReBowl();
+
+        cache.size();
+    });
+});
